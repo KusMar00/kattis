4980cbb7ef3adf2ad9e01e4b6fb4ae1676662b97
Markus Johansen <markusraunjohansen@gmail.com>
2023-04-24 23:04:40 +0200

    script working

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Kattis
 | Problem | Solutions | Difficulty |
 | ------- | --------- | ---------- |
-| [99 Problems](https://open.kattis.com/problems) |  - | 0.0 |
-| [Knight Packing](https://open.kattis.com/problems) |  - | 0.0 |
-| [Metronome](https://open.kattis.com/problems) |  - | 0.0 |
-| [Logic Functions](https://open.kattis.com/problems) |  - | 0.0 |
+| [Logic Functions](https://open.kattis.com/problems/logicfunctions) | [C++](https://github.com/KusMar00/kattis/tree/main/solutions/Logic%20Functions/logicfunctions.cpp) | 1.6 |
+| [Knight Packing](https://open.kattis.com/problems/knightpacking) | [C](https://github.com/KusMar00/kattis/tree/main/solutions/Knight%20Packing/knightpacking.c) | 1.3 |
+| [99 Problems](https://open.kattis.com/problems/99problems) | [C](https://github.com/KusMar00/kattis/tree/main/solutions/99%20Problems/99problems.c) | 2.8 |
+| [Metronome](https://open.kattis.com/problems/metronome) | [C](https://github.com/KusMar00/kattis/tree/main/solutions/Metronome/metronome.c) | 1.3 |
--- update_readme.py
+++ update_readme.py
@@ -6,35 +6,92 @@ from bs4 import BeautifulSoup
 
 # Gives a link to the problem description from Kattis
 def get_problem_url(problem):
-    url = "[{title}](https://open.kattis.com/problems)"
-    return url.format(title = problem)
+    url = f"https://open.kattis.com/problems/{problem}"
+    return url
+
+def get_solution_url(title, solution, extension):
+    title = title.replace(' ', '%20')
+    url = f"[{extension}](https://github.com/KusMar00/kattis/tree/main/solutions/{title}/{solution})"
+    return url
 
 # Scrapes problem info from Kattis
 def scrape_problem(problem_url):
-    page = requests.get(problem_url+'?tab=metadata')
-    soup = BeautifulSoup(page.content ,'html.parser')
-    spans = soup.find_all("span", {"class": "difficulty_number"})
-    number = spans[0].text
-    print(number)
-    return
+    try:
+        page = requests.get(problem_url+'?tab=metadata')
+        soup = BeautifulSoup(page.content ,'html.parser')
+        number = str(soup.find_all("span", {"class": "difficulty_number"})[0].text)
+        return number
+    except:
+        return ""
 
-# Gives a list of my solutions to the given problem
-def list_solutions(problem):
-    return
+
+
+# Gives a dictionary with metadata on a given problem
+def get_problem_info(problem):
+    problem_dict = {"title": problem, "solutions": [], "origin": ""}
+    solutions = [ x[2] for x in os.walk(f"./solutions/{problem}") ]
+    # Find valid solutions
+    for solution in solutions[0]:
+        split = solution.split(".")
+        if problem_dict["origin"] == "":
+            problem_dict["origin"] = split[0]
+        ext = split[-1]
+        if ext == "c":
+            url = get_solution_url(problem, solution, "C")
+            problem_dict["solutions"].append(url)
+        elif ext == "cpp":
+            url = get_solution_url(problem, solution, "C++")
+            problem_dict["solutions"].append(url)
+        elif ext == "py":
+            url = get_solution_url(problem, solution, "Python")
+            problem_dict["solutions"].append(url)
+        elif ext == "java":
+            url = get_solution_url(problem, solution, "Java")
+            problem_dict["solutions"].append(url)
+        elif ext == "go":
+            url = get_solution_url(problem, solution, "Go")
+            problem_dict["solutions"].append(url)
+        elif ext == "fs" or ext == "fsx" or ext == "fsi":
+            url = get_solution_url(problem, solution, "F#")
+            problem_dict["solutions"].append(url)
+        elif ext == "cs":
+            url = get_solution_url(problem, solution, "C#")
+            problem_dict["solutions"].append(url)
+    
+    problem_dict["url"] = get_problem_url(problem_dict["origin"])
+    print(problem_dict["url"] )
+    problem_dict["difficulty"] = scrape_problem(problem_dict["url"])
+
+    return problem_dict
 
 def write_readme():
-    problems = [ x[1] for x in os.walk("./solutions") ]
+    problems = [ x[1] for x in os.walk("./solutions/") ]
     with open("README.md", "w") as f:
         f.write("# Kattis\n")
         f.write("| Problem | Solutions | Difficulty |\n")
         f.write("| ------- | --------- | ---------- |\n")
-        for problem in problems:
-            f.write("| " + problem_url(problem) + " | ")
-            f.write(" - | 0.0 |\n")
+        for problem in problems[0]:
+            problem_dict = get_problem_info(problem)
+            url = problem_dict["url"]
 
-def main():
-    write_readme()
+            # Write problem title
+            title = problem_dict["title"]
+            url = problem_dict["url"]
+            f.write(f"| [{title}]({url}) | ")
 
+            # Write solutions
+            solutions = problem_dict["solutions"]
+            for solution in solutions:
+                if solution == solutions[-1]:
+                    print(solution)
+                    f.write(solution)
+                else:
+                    f.write(solution + ", ")
+            f.write(" |")
+
+            # Write difficulty rating
+            diff = problem_dict["difficulty"]
+            f.write(f" {diff} |\n")
 
 if __name__ == '__main__':
     write_readme()
